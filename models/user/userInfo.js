3df6fdda7bbae6f04a2de0a5c02f77c56594e5e9
QuangAnhTo <quangato072@gmail.com>
2024-05-09 10:33:21 +0700

    add user information model

---
+++ models/user/userInfo.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const useInfoSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    dob: {
+        type: Date,
+        required: true,
+    },
+    nation: {
+        type: String,
+        required: true,
+    },
+    hometown: {
+        type: String,
+        required: true,
+    },
+    education: String,
+});
+
+const UserInfoModel = mongoose.model("UserInfo", useInfoSchema);
+
+export default UserInfoModel;
