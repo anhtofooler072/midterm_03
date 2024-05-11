2f56c654539a021a2f0fa91aac6d692c8f3bf7d5
QuangAnhTo <quangato072@gmail.com>
2024-05-11 13:20:00 +0700

    add user info functions

--- controllers/userinfo.js
+++ controllers/userinfo.js
@@ -1,39 +1,39 @@
 import UserInfoModel from "../models/user/userInfo.js";
 
 const userInfoController = {
-    createbio: async (req, res) => {
-        try {
-            const { userId, name, dob, nation, hometown, education } = req.body;
-            const userInfo = new UserInfoModel({
-                userId,
-                name,
-                dob,
-                nation,
-                hometown,
-                education,
-            });
-            await userInfo.save();
-            res.status(201).send(userInfo);
-        } catch (error) {
-            res.status(400).send(error);
-        }
-    },
-    updatebio: async (req, res) => {
-        try {
-            const { userId } = req.params;
-            const { name, dob, nation, hometown, education } = req.body;
-            const userInfo = await UserInfoModel.findOne({ userId });
-            userInfo.name = name;
-            userInfo.dob = dob;
-            userInfo.nation = nation;
-            userInfo.hometown = hometown;
-            userInfo.education = education;
-            await userInfo.save();
-            res.status(200).send(userInfo);
-        } catch (error) {
-            res.status(400).send(error);
-        }
-    },
+  createbio: async (req, res) => {
+    try {
+      const { userId, name, dob, nation, hometown, education } = req.body;
+      const userInfo = new UserInfoModel({
+        userId,
+        name,
+        dob,
+        nation,
+        hometown,
+        education,
+      });
+      await userInfo.save();
+      res.status(201).send(userInfo);
+    } catch (error) {
+      res.status(400).send(error);
+    }
+  },
+  updatebio: async (req, res) => {
+    try {
+      const { userId } = req.params;
+      const { name, dob, nation, hometown, education } = req.body;
+      const userInfo = await UserInfoModel.findOne({ userId });
+      userInfo.name = name;
+      userInfo.dob = dob;
+      userInfo.nation = nation;
+      userInfo.hometown = hometown;
+      userInfo.education = education;
+      await userInfo.save();
+      res.status(200).send(userInfo);
+    } catch (error) {
+      res.status(400).send(error);
+    }
+  },
 };
 
 export default userInfoController;
