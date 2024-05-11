15bc78bf2f74dc7bb88c7308ae4a01c804ede4ca
QuangAnhTo <quangato072@gmail.com>
2024-05-11 13:07:11 +0700

    chore: Update server configuration and dependencies

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import rootRoutes from './routes/user/rootroutes.js';
+import express from "express";
+import mongoose from "mongoose";
+import rootRoutes from "./routes/user/rootroutes.js";
+import dotenv from "dotenv";
 /*------------------ DIVISION -----------------*/
 
-const PORT = 3000;
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+const mongoUri = process.env.MONGO_URI;
+
+await mongoose
+  .connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connect to MongoDB successfully");
+  })
+  .catch((error) => {
+    console.log("Error connecting to MongoDB");
+    console.log(error);
+  });
 
 const app = express();
 app.use(express.json());
 app.use("/quanganh/api", rootRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server hẳn là đang chạy ở cổng ${PORT}`);
 });
