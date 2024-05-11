534124539f6376d36f3586075a71ea8b32d6504c
QuangAnhTo <quangato072@gmail.com>
2024-05-11 13:45:25 +0700

    chore: Add authentication middleware for token verification

---
+++ middlewares/auth.js
@@ -0,0 +1,23 @@
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const auth = {
+    verifyToken: (req, res, next) => {
+        const token = req.headers['x-access-token'];
+        if (!token) {
+        return res.status(403).send({ message: 'No token provided!' });
+        }
+    
+        jwt.verify(token, process.env.SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ message: 'Unauthorized!' });
+        }
+        req.userId = decoded.id;
+        next();
+        });
+    }
+};
+
+export default auth;
